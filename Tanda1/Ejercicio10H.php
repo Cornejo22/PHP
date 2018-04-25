82decb0ab3b616d164bccc82d938212aea9f4b7b
Cornejo22 <miguel.cornejo.manzanares@gmail.com>
2018-04-25 21:57:53 +0200

    Subida de nuevos Ejercicios

--- Tanda1/Ejercicio10H.php
+++ Tanda1/Ejercicio10H.php
@@ -5,6 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ejercicio 10</title>
+    <style>
+    
+    a{
+        text-decoration: none;
+        color:black;
+    }
+    a:hover{
+        color:red;
+    }
+
+    </style>
 </head>
 <body>
 
@@ -16,12 +27,27 @@ La base de la pirámide debe estar formada por 9 asteriscos.
 
 <?php
 
-    $nEStrellitas = $_POST["estrellitas"];
-    for ($i=0; $i <$nEStrellitas ; $i++) { 
-        # code...
+$altura = $_POST["estrellitas"];
+$lineas = $altura + 4;
+for ($i = 0; $i < $altura; $i++) 
+{
+    for ($j = 0; $j < $lineas; $j++) 
+    {
+        if ($j == 0 || $j == $lineas) 
+        {
+            echo " ";
+        }
+        else
+        {
+            echo "*";    
+        }
     }
+    echo "<br/>";
+}
 
 ?>
+
+<button><a href="Ejercicio10.php">Volver a calcular estrellitas</a></button>
     
 </body>
 </html>
